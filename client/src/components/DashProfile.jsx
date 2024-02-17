e5dd2a76918fd90a10697f1563b5abad4a404390
Jayandra Hamal <salonhamal278@gmail.com>
2024-02-17 22:10:07 +0530

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -15,7 +15,8 @@ import {
   updateFailure,
   deleteUserStart,
   deleteUserSuccess,
-  deleteUserFailure
+  deleteUserFailure,
+  signoutSuccess
 } from '../redux/user/userSlice'
 
 
@@ -152,6 +153,23 @@ const DashProfile = () => {
     }
   }
 
+
+  const handleSignout = async() => {
+    try {
+      const res = await fetch ('/api/user/signout', {
+        method: 'POST'
+      });
+      const data = await res.json();
+      if(!res.ok) {
+        console.log(data.message)
+      } else {
+        dispatch(signoutSuccess())
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>profile</h1>
@@ -204,7 +222,7 @@ const DashProfile = () => {
 
       <div className='text-red-500 flex justify-between mt-5' >
         <span className='cursor-pointer' onClick={() => setShowModal(true)}>Delete Account</span>
-        <span className='cursor-pointer'>Sign Out</span>
+        <span  onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
 
       </div>
 
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { Sidebar } from 'flowbite-react'
 import { HiArrowSmRight, HiUser } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
+import {signoutSuccess} from '../redux/user/userSlice'
+import {useDispatch} from 'react-redux'
 
 const DashSidebar = () => {
     const location = useLocation()
 
     const [tab, setTab] = useState('')
+
+    const dispatch = useDispatch()
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search)
@@ -15,6 +19,23 @@ const DashSidebar = () => {
             setTab(tabFromUrl)
         }
     }, [location.search])
+
+    const handleSignout = async() => {
+        try {
+          const res = await fetch ('/api/user/signout', {
+            method: 'POST'
+          });
+          const data = await res.json();
+          if(!res.ok) {
+            console.log(data.message)
+          } else {
+            dispatch(signoutSuccess())
+          }
+        } catch (error) {
+          console.log(error.message)
+        }
+      }
+
     return (
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
@@ -28,7 +49,7 @@ const DashSidebar = () => {
 
                     </Link>
 
-                    <Sidebar.Item icon={HiArrowSmRight} lclassname="cursor-pointer">
+                    <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} classname="cursor-pointer">
                         Sign Out
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
