36c54fd2791f157988c45706f9558e3678442112
Jayandra Hamal <salonhamal278@gmail.com>
2024-02-20 16:21:24 +0530

    Add posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,61 +1,86 @@
 import React, { useEffect, useState } from 'react'
 import { Sidebar } from 'flowbite-react'
-import { HiArrowSmRight, HiUser } from 'react-icons/hi'
+import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
-import {signoutSuccess} from '../redux/user/userSlice'
-import {useDispatch} from 'react-redux'
+import { signoutSuccess } from '../redux/user/userSlice'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const DashSidebar = () => {
-    const location = useLocation()
+  const location = useLocation()
 
-    const [tab, setTab] = useState('')
+  const [tab, setTab] = useState('')
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(location.search)
-        const tabFromUrl = urlParams.get('tab')
-        if (tabFromUrl) {
-            setTab(tabFromUrl)
-        }
-    }, [location.search])
+  const {currentUser} = useSelector(state => state.user)
 
-    const handleSignout = async() => {
-        try {
-          const res = await fetch ('/api/user/signout', {
-            method: 'POST'
-          });
-          const data = await res.json();
-          if(!res.ok) {
-            console.log(data.message)
-          } else {
-            dispatch(signoutSuccess())
-          }
-        } catch (error) {
-          console.log(error.message)
-        }
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [location.search])
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST'
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message)
+      } else {
+        dispatch(signoutSuccess())
       }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
-    return (
-        <Sidebar className='w-full md:w-56'>
-            <Sidebar.Items>
-                <Sidebar.ItemGroup>
-                    <Link to='/dashboard?tab=profile' >
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          <Link to='/dashboard?tab=profile' >
 
 
-                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark" as='div'>
-                            Profile
-                        </Sidebar.Item>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor="dark"
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
 
-                    </Link>
+          </Link>
 
-                    <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} classname="cursor-pointer">
-                        Sign Out
-                    </Sidebar.Item>
-                </Sidebar.ItemGroup>
-            </Sidebar.Items>
-        </Sidebar>
-    )
+          {/* only available for admin */}
+          {
+            currentUser.isAdmin && (
+              <Link to='/dashboard?tab=posts'>
+                <Sidebar.Item
+                  active={tab === 'posts'}
+                  icon={HiDocumentText}
+                  as='div'
+                >
+                  Posts
+                </Sidebar.Item>
+              </Link>
+            )
+          }
+
+
+          <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} classname="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
 }
 
 export default DashSidebar 
