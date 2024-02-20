027ac135e1f73ad08329721feeff2e9991e8bd18
Jayandra Hamal <salonhamal278@gmail.com>
2024-02-20 08:42:41 +0530

    complete upload post image functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
@@ -31,6 +32,7 @@ const App = () => {
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route  path='/create-post' element={<CreatePost />} />
         </Route>
+
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
