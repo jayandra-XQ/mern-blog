9fd63235b2a7c2b78dd2597db2c88b08931b4b11
Jayandra Hamal <salonhamal278@gmail.com>
2024-02-21 21:33:10 +0530

    add delete post functionality to the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,7 +1,9 @@
-import { Table, TableCell } from 'flowbite-react'
+import { Button, Modal, Table, TableCell } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { HiOutlineExclamationCircle } from "react-icons/hi2";
+
 
 const DashPosts = () => {
 
@@ -9,8 +11,10 @@ const DashPosts = () => {
   const [userPosts, setUserPosts] = useState([])
 
   const [showMore, setShowMore] = useState(true)
+  const [showModal, setShowModal] = useState(false)
+  const [postIdToDelete, setPostIdToDelete] = useState(' ')
 
- 
+
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -21,7 +25,7 @@ const DashPosts = () => {
         if (res.ok) {
           setUserPosts(data.posts);
 
-          if(data.posts.length < 9) {
+          if (data.posts.length < 9) {
             setShowMore(false)
           }
         }
@@ -39,14 +43,35 @@ const DashPosts = () => {
     const startIndex = userPosts.length;
 
     try {
-      const res = await fetch (`api/poost/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const res = await fetch(`api/poost/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
       const data = await res.json();
 
-      if(res.ok) {
+      if (res.ok) {
         setUserPosts((prev) => [...prev, ...data.posts]);
-        if(data.posts.length < 9) {
+        if (data.posts.length < 9) {
           setShowMore(false)
         }
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
+  const handleDeletePost = async () => {
+    setShowModal(false)
+
+    try {
+      const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`, {
+        method: 'DELETE'
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message)
+      } else {
+        setUserPosts((prev) =>
+          prev.filter((post) => post._id !== postIdToDelete)
+        )
       }
     } catch (error) {
       console.log(error.message)
@@ -98,7 +123,10 @@ const DashPosts = () => {
                       </Table.Cell>
 
                       <Table.Cell>
-                        <span className='font-medium text-red-500 hover:underline'>
+                        <span onClick={() => {
+                          setShowModal(true)
+                          setPostIdToDelete(post._id)
+                        }} className='font-medium text-red-500 hover:underline'>
                           Delete
                         </span>
                       </Table.Cell>
@@ -121,13 +149,38 @@ const DashPosts = () => {
                   Show more
                 </button>
               )
-              }
-            
+            }
+
           </>
         ) : (
           <p>You have no posts yet</p>
-        )
-      }
+        )}
+
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-500'>Are you sure you want to delete your post? </h3>
+
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeletePost}>
+                Yes, I'm sure
+              </Button>
+
+              <Button color='gray'
+                onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
