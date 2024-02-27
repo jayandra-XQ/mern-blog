50e8c1921a432a130393770d0fe1c29ae116fa4d
Jayandra Hamal <salonhamal278@gmail.com>
2024-02-27 23:44:27 +0530

    Add recent article section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom'
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 
 export default function PostPage() {
@@ -11,6 +12,8 @@ export default function PostPage() {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const [post, setPost] = useState(null)
+
+  const [recentPosts, setRecentPosts] = useState(null)
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -37,6 +40,21 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
 
 
   if (loading)
@@ -80,6 +98,14 @@ export default function PostPage() {
   
     </div>
       <CommentSection postId={post._id} />
+
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className=''>
+        {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 }
