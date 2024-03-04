4ebb924c3ee63c9105f8b8473202ec2162687d78
Jayandra Hamal <salonhamal278@gmail.com>
2024-03-04 21:16:34 +0530

    updated footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -81,10 +81,10 @@ const FooterCom = () => {
 
                     <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
                         <Footer.Icon   href='#' icon={BsFacebook}/>
-                        <Footer.Icon   href='#' icon={BsInstagram}/>
-                        <Footer.Icon   href='#' icon={BsTwitter}/>
-                        <Footer.Icon   href='#' icon={BsGithub}/>
-                        <Footer.Icon   href='#' icon={FaLinkedin}/>
+                        <Footer.Icon   href='https://www.instagram.com/__jayandraa__/' icon={BsInstagram}/>
+                        <Footer.Icon   href='https://twitter.com/jayandra_hamal' icon={BsTwitter}/>
+                        <Footer.Icon   href='https://github.com/jayandra-XQ' icon={BsGithub}/>
+                        <Footer.Icon   href='https://www.linkedin.com/in/jayandra-hamal-ba798a241/' icon={FaLinkedin}/>
                     </div>
                 </div>
             </div>
